1bd13440e2bf13a733b45ee497fa949ba9275bce
alinka-nabieva <290606na@gmail.com>
2025-04-09 09:17:51 +0300

    Update js6.js

--- js/js6.js
+++ js/js6.js
@@ -1,4 +1,4 @@
 'use strict';
-let checkAge = (age) => { (age > 18) ? true : confirm('Родители разрешили?') };
+let checkAge = (age) => { (age > 18) ? alert(true) : alert(confirm('Родители разрешили?')) };
 let age = prompt('Ведите свой возраст');
 checkAge(age);
